4637a255f81b7da454294d93ffb6d281d0b0572c
akashgupta222 <akii.gupta16@gmail.com>
2016-10-05 20:53:48 +0630

    changed submission name

--- averager.py
+++ averager.py
@@ -7,4 +7,4 @@ sub3=pd.read_csv('intermediate_keras.csv')
 #take average and save
 pred2=(sub1.Footfall+sub2.Footfall+sub3.Footfall)/3.0
 sub1.Footfall=pred2
-sub1.to_csv('submission.csv',index=False)
+sub1.to_csv('final prediction.csv',index=False)
